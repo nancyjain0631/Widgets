76b6a35cb93b9f9df8a37ca5ffe03977788114d6
Nancy Jain <nancyjain0631@gmail.com>
2021-01-26 22:08:48 +0530

    2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import Accordion from './components/Accordion';
 
-export default () =>{
-    return <h1>Widgets App</h1>;
-}
+const items=[
+    {
+        title:'What is React?',
+        content:'React is a front end JS framework.'
+    },
+    {
+        title:'Why use React?',
+        content:'React is a favourite JS library among engineers.'
+    },
+    {
+        title:'How do you use React?',
+        content:'You can use React by creating components.'
+    }
+];
+
+export default () =>(
+    <div>
+        <Accordion items={items} />
+    </div>
+);

c04e5d0542aefe0f5f4d29d661ffe5206357eb67
Nancy Jain <nancyjain0631@gmail.com>
2021-02-17 23:22:18 +0530

    3

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,13 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 const Search = () =>{
-    const [term, setTerm] = useState('');
-    
+    const [term, setTerm] = useState('programming');
+    const [results, setResults]= useState([]);
+    console.log(results);
     useEffect(() => {
         const search= async ()=>{
-            await axios.get('https://en.wikipedia.org/w/api.php',{
+            
+            const {data}= await axios.get('https://en.wikipedia.org/w/api.php',{
             params:{
                 action:'query',
                 list:'search',
@@ -13,7 +16,9 @@ const Search = () =>{
                 srsearch: term,
             }
             });
+            setResults(data.query.search);
         };
+        search();
     },[term]);
     return (
         <div>

a7b97c54aeda876bdfc944698ecfe0bcf503f4ac
Nancy Jain <nancyjain0631@gmail.com>
2021-02-17 23:03:02 +0530

    4

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,20 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 const Search = () =>{
     const [term, setTerm] = useState('');
+    
+    useEffect(() => {
+        const search= async ()=>{
+            await axios.get('https://en.wikipedia.org/w/api.php',{
+            params:{
+                action:'query',
+                list:'search',
+                origin: '*',
+                format: 'json',
+                srsearch: term,
+            }
+            });
+        };
+    },[term]);
     return (
         <div>
             <div className="ui form">

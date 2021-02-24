663deb4b9542664e2d608eaa4a869499fefe6165
Nancy Jain <nancyjain0631@gmail.com>
2021-02-24 23:34:23 +0530

    2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 const items=[
     {
         title:'What is React?',
@@ -15,10 +16,24 @@ const items=[
         content:'You can use React by creating components.'
     }
 ];
+const options= [
+    {
+        label:'The Color Red',
+        value:'red'
+    },
+    {
+        label:'The Color Green',
+        value:'green'
+    },
+    {
+        label:'A Shade of Blue',
+        value:'blue'
+    }
 
+];
 export default () =>(
     <div>
-        {/* <Accordion items={items} /> */}
-        <Search/>
+        
+        <Dropdown options={options} />
     </div>
 );
---
+++ src/components/Dropdown.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const Dropdown = () =>{
+    return <h1>Dropdown</h1>;
+};
+export default Dropdown;

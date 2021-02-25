76a58f451c75b3c5ec1c2e645ad1feb066a1543c
Nancy Jain <nancyjain0631@gmail.com>
2021-02-25 10:59:20 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
@@ -32,8 +32,12 @@ const options= [
 
 ];
 export default () =>(
+    const [selected, setSelected] =useState(options[0]);
     <div>
         
-        <Dropdown options={options} />
+        <Dropdown selected={selected}
+                  onSelectedChange={setSelected}
+                  options={options}
+        />
     </div>
 );
